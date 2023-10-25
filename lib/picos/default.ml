6d8eb1326f7c2c46fb8139cdd307228106da7715
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-10-26 00:59:21 +0300

    Refactor to move Per_thread inside Default module

--- lib/picos/default.ml
+++ lib/picos/default.ml
@@ -1,4 +1,27 @@
 open struct
+  module Per_thread = struct
+    type t = {
+      mutex : Mutex.t;
+      condition : Condition.t;
+      fiber : [ `Sync | `Async ] Bootstrap.Fiber.t;
+    }
+
+    open struct
+      let create fiber =
+        let mutex = Mutex.create () and condition = Condition.create () in
+        { mutex; condition; fiber }
+
+      let per_thread_key =
+        TLS.new_key @@ fun () ->
+        create
+          (Bootstrap.Fiber.create ~forbid:false
+             (Bootstrap.Computation.create ()))
+    end
+
+    let get () = TLS.get per_thread_key
+    let set fiber = TLS.set per_thread_key (create fiber)
+  end
+
   let release _ _ (per_thread : Per_thread.t) =
     Mutex.lock per_thread.mutex;
     Mutex.unlock per_thread.mutex;
