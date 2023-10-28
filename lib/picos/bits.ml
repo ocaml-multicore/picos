d4bc6101ec6b42ebdbe75ddff33da1adcedd7308
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-10-29 01:55:25 +0300

    Use thread-local-storage package

--- lib/picos/bits.ml
+++
@@ -1,7 +0,0 @@
-let ceil_pow_2_minus_1 n =
-  let n = n lor (n lsr 1) in
-  let n = n lor (n lsr 2) in
-  let n = n lor (n lsr 4) in
-  let n = n lor (n lsr 8) in
-  let n = n lor (n lsr 16) in
-  if Sys.int_size > 32 then n lor (n lsr 32) else n
