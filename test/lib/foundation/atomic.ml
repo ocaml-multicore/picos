bc8a6089c34bec0a2abbd7a34466b7e7c4123749
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-10-30 14:47:16 +0200

    No need to use `Atomic` within a single domain

--- test/lib/foundation/atomic.ml
+++
@@ -1,8 +0,0 @@
-include Stdlib.Atomic
-
-let rec update t fn =
-  let before = Stdlib.Atomic.get t in
-  let after = fn before in
-  if Stdlib.Atomic.compare_and_set t before after then before else update t fn
-
-let modify t fn = update t fn |> ignore
