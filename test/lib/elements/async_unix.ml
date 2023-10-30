dff22aaffdfc6fd8096be920b89b9d9352f5b19b
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-10-30 14:00:58 +0200

    More careful init

--- test/lib/elements/async_unix.ml
+++ test/lib/elements/async_unix.ml
@@ -26,20 +26,24 @@ module Async = struct
 
   type state = {
     mutable state : [ `Init | `Locked | `Alive | `Dead ];
+    mutable pipe_inn : Unix.file_descr;
     mutable pipe_out : Unix.file_descr;
+    mutable exn_bt : Exn_bt.t;
     reading : Awaiter.t list Atomic.t;
     writing : Awaiter.t list Atomic.t;
   }
 
   let key =
     (* In this case we really want to use DLS rather than TLS. *)
+    (* Unfortunately we cannot safely allocate a pipe here, so we use stdin
+       as a dummy value. *)
+    let dummy_pipe = Unix.stdin in
     DLS.new_key @@ fun () ->
     {
       state = `Init;
-      pipe_out =
-        (* Unfortunately we cannot safely allocate a pipe here, so we use stdin
-           as a dummy value. *)
-        Unix.stdin;
+      pipe_inn = dummy_pipe;
+      pipe_out = dummy_pipe;
+      exn_bt = Exn_bt.get_callstack 0 Exit;
       reading = Atomic.make [];
       writing = Atomic.make [];
     }
@@ -51,60 +55,72 @@ module Async = struct
          true
        end
 
-  let needs_init s = s.state != `Alive
-
-  let[@poll error] [@inline never] unlock s pipe_out =
-    s.pipe_out <- pipe_out;
-    s.state <- `Alive
+  let[@poll error] [@inline never] unlock s state = s.state <- state
 
   let wakeup s =
     let n = Unix.write s.pipe_out (Bytes.create 1) 0 1 in
     assert (n = 1)
 
-  let rec init s =
+  let io_thread s =
+    begin
+      try
+        (* The pipe is used to wake up the select after changing the lists of
+           reading and writing file descriptors. *)
+        let pipe_inn, pipe_out = Unix.pipe ~cloexec:true () in
+        s.pipe_inn <- pipe_inn;
+        s.pipe_out <- pipe_out;
+        unlock s `Alive;
+        (* This is the IO select loop that performs select and then wakes up
+           fibers blocked on IO. *)
+        while s.state == `Alive do
+          let rs, ws, _ =
+            Unix.select
+              (s.pipe_inn
+              :: List.map Awaiter.file_descr_of (Atomic.get s.reading))
+              (List.map Awaiter.file_descr_of (Atomic.get s.writing))
+              [] (-1.0)
+          in
+          List.iter
+            (Awaiter.signal_or_wakeup s.pipe_inn (Atomic.get s.reading))
+            rs;
+          List.iter (Awaiter.signal (Atomic.get s.writing)) ws;
+          Atomic.modify s.reading (List.fold_right Awaiter.reject rs);
+          Atomic.modify s.writing (List.fold_right Awaiter.reject ws)
+        done
+      with exn -> s.exn_bt <- Exn_bt.get exn
+    end;
+    unlock s `Dead;
+    if s.pipe_inn != Unix.stdin then Unix.close s.pipe_inn;
+    if s.pipe_out != Unix.stdin then Unix.close s.pipe_out
+
+  let start s =
     (* DLS initialization may be run multiple times, so we perform more involved
        initialization here. *)
-    if try_lock s then begin
-      (* The pipe is used to wake up the select after changing the lists of
-         reading and writing file descriptors. *)
-      let pipe_inn, pipe_out = Unix.pipe ~cloexec:true () in
-      unlock s pipe_out;
-      let t =
-        ()
-        |> Thread.create @@ fun () ->
-           (* This is the IO select loop that performs select and then wakes up
-              fibers blocked on IO. *)
-           while s.state != `Dead do
-             let rs, ws, _ =
-               Unix.select
-                 (pipe_inn
-                 :: List.map Awaiter.file_descr_of (Atomic.get s.reading))
-                 (List.map Awaiter.file_descr_of (Atomic.get s.writing))
-                 [] (-1.0)
-             in
-             List.iter
-               (Awaiter.signal_or_wakeup pipe_inn (Atomic.get s.reading))
-               rs;
-             List.iter (Awaiter.signal (Atomic.get s.writing)) ws;
-             Atomic.modify s.reading (List.fold_right Awaiter.reject rs);
-             Atomic.modify s.writing (List.fold_right Awaiter.reject ws)
-           done;
-           Unix.close pipe_inn;
-           Unix.close pipe_out
-      in
-      Domain.at_exit @@ fun () ->
-      s.state <- `Dead;
-      wakeup s;
-      Thread.join t
-    end
-    else if needs_init s then begin
-      Thread.yield ();
-      init s
-    end
+    match Thread.create io_thread s with
+    | thread ->
+        Domain.at_exit @@ fun () ->
+        unlock s `Dead;
+        wakeup s;
+        Thread.join thread;
+        if s.exn_bt.exn != Exit then Exn_bt.raise s.exn_bt
+    | exception exn ->
+        unlock s `Dead;
+        raise exn
+
+  let wait s =
+    while s.state == `Locked do
+      Thread.yield ()
+    done;
+    if s.state != `Alive then
+      invalid_arg "Async_unix: domain has been terminated"
+
+  let init s =
+    if try_lock s then start s;
+    wait s
 
   let get () =
     let s = DLS.get key in
-    if needs_init s then init s;
+    if s.state != `Alive then init s;
     s
 
   let await s r file_descr =
