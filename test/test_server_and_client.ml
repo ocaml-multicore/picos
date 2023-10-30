0f8aa8a92ae9a8ba78acc3399bb2d027dc7b4213
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-10-30 16:05:23 +0200

    Let system pick server port

--- test/test_server_and_client.ml
+++ test/test_server_and_client.ml
@@ -4,11 +4,7 @@ open Elements
 let main () =
   Bundle.run @@ fun bundle ->
   let n = 100 in
-  let port =
-    Random.self_init ();
-    Random.int 1000 + 3000
-  in
-  let server_addr = Async_unix.(ADDR_INET (inet_addr_loopback, port)) in
+  let server_addr = ref None in
 
   let server =
     Bundle.fork bundle @@ fun () ->
@@ -20,7 +16,8 @@ let main () =
         Async_unix.socket ~cloexec:true PF_INET SOCK_STREAM 0
       in
       Async_unix.set_nonblock socket;
-      Async_unix.bind socket server_addr;
+      Async_unix.bind socket Async_unix.(ADDR_INET (inet_addr_loopback, 0));
+      server_addr := Some (Async_unix.getsockname socket);
       Async_unix.listen socket 1;
       Printf.printf "  Server listening\n%!";
       Async_unix.accept ~cloexec:true socket |> fst
@@ -40,16 +37,18 @@ let main () =
       finally Async_unix.close @@ fun () ->
       Async_unix.socket ~cloexec:true PF_INET SOCK_STREAM 0
     in
-    let retries = ref 100 in
-    while
-      match Async_unix.connect socket server_addr with
-      | () -> false
-      | exception Async_unix.Unix_error (ECONNREFUSED, _, _) ->
-          decr retries;
-          0 <= !retries || failwith "Could not connect to server"
-    do
-      Sleep.sleepf 0.01
-    done;
+    let server_addr =
+      let rec loop retries =
+        match !server_addr with
+        | None ->
+            if retries < 0 then failwith "No server address";
+            Sleep.sleepf 0.01;
+            loop (retries - 1)
+        | Some addr -> addr
+      in
+      loop 100
+    in
+    Async_unix.connect socket server_addr;
     Printf.printf "  Client connected\n%!";
     Async_unix.set_nonblock socket;
     let bytes = Bytes.create n in
