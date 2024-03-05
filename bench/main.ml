a02278ddedc21c6d217e04c8a52845d65f5ba0a5
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-05 02:38:14 +0200

    Add benchmark of `FLS` excluding cost of `Current`

--- bench/main.ml
+++ bench/main.ml
@@ -2,6 +2,7 @@ let benchmarks =
   [
     ("Picos Computation", Bench_computation.run_suite);
     ("Picos Current", Bench_current.run_suite);
+    ("Picos FLS (excluding Current)", Bench_fls_excluding_current.run_suite);
     ("Foundation Mpsc_queue", Bench_mpsc_queue.run_suite);
   ]
 
