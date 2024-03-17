14086c229299a089dc4db664a323c6f1f0d7f016
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-17 23:07:50 +0200

    Add (private) `Picos_thread_atomic` library

--- lib/picos/thread_atomic/picos_thread_atomic.mli
+++ lib/picos/thread_atomic/picos_thread_atomic.mli
@@ -3,10 +3,4 @@
     ⚠️ These operations are not parallelism safe. *)
 
 val compare_and_set : 'a ref -> 'a -> 'a -> bool
-(** *)
-
-val update : 'a ref -> ('a -> 'a) -> 'a
-(** *)
-
-val modify : 'a ref -> ('a -> 'a) -> unit
-(** *)
+val exchange : 'a ref -> 'a -> 'a
