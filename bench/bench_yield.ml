42cd31d106390fcf72290bfbfc89f94c31d11719
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-22 20:45:31 +0200

    Add `yield` benchmark

---
+++ bench/bench_yield.ml
@@ -0,0 +1,36 @@
+open Multicore_bench
+open Picos
+
+let factor =
+  Util.iter_factor
+  * if String.starts_with ~prefix:"4." Sys.ocaml_version then 1 else 10
+
+let run_one ~budgetf ~n_fibers () =
+  let n_yields = 100 * factor in
+  let n_yields_per_fiber = n_yields / n_fibers in
+
+  let init _ = () in
+  let wrap _ () = Scheduler.run in
+  let work _ () =
+    let n_live = Atomic.make n_fibers in
+    let computation = Computation.create () in
+    List.init n_fibers (fun _ () ->
+        for _ = 1 to n_yields_per_fiber do
+          Fiber.yield ()
+        done;
+        if 1 = Atomic.fetch_and_add n_live (-1) then
+          Computation.finish computation)
+    |> Fiber.spawn ~forbid:false computation;
+    Computation.wait computation
+  in
+
+  let config =
+    Printf.sprintf "%d fiber%s" n_fibers (if n_fibers = 1 then "" else "s")
+  in
+  Times.record ~budgetf ~n_domains:1 ~init ~wrap ~work ()
+  |> Times.to_thruput_metrics ~n:n_yields ~singular:"yield" ~config
+
+let run_suite ~budgetf =
+  [ 1; 10; 100; 1_000; 10_000 ]
+  |> List.concat_map @@ fun n_fibers ->
+     if n_fibers <= factor then run_one ~budgetf ~n_fibers () else []
