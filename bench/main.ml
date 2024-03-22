00cdeef0d2b7f3e2a946ba97cdef01c9a9773775
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-22 20:45:31 +0200

    Add `yield` benchmark

--- bench/main.ml
+++ bench/main.ml
@@ -3,6 +3,7 @@ let benchmarks =
     ("Picos Computation", Bench_computation.run_suite);
     ("Picos Current", Bench_current.run_suite);
     ("Picos FLS (excluding Current)", Bench_fls_excluding_current.run_suite);
+    ("Picos Yield", Bench_yield.run_suite);
     ("Foundation Mpsc_queue", Bench_mpsc_queue.run_suite);
   ]
 
