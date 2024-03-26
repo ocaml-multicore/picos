9550986142659b9d4f88d3ad0aa65e1c6e31f19f
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-03-26 19:21:20 +0200

    On Win32 `Unix.select` timeout seems to have really bad resolution

--- bench/bench_cancel_after.ml
+++ bench/bench_cancel_after.ml
@@ -5,7 +5,7 @@ let exit_exn_bt = Exn_bt.get_callstack 0 Exit
 let seconds = 0.000_000_001 (* 1 ns *)
 
 let run_round_trip ~budgetf ~n_domains () =
-  let n_ops = 5 * Util.iter_factor * n_domains in
+  let n_ops = if Sys.win32 then 100 else 5 * Util.iter_factor * n_domains in
 
   let n_ops_todo = Atomic.make 0 |> Multicore_magic.copy_as_padded in
 
@@ -36,7 +36,7 @@ let run_round_trip ~budgetf ~n_domains () =
   |> Times.to_thruput_metrics ~n:n_ops ~singular:"round-trip" ~config
 
 let run_async ~budgetf ~n_domains () =
-  let n_ops = 50 * Util.iter_factor * n_domains in
+  let n_ops = if Sys.win32 then 1_000 else 50 * Util.iter_factor * n_domains in
 
   let n_ops_todo = Atomic.make 0 |> Multicore_magic.copy_as_padded in
 
