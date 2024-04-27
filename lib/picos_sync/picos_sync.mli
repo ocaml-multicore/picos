0d2e8aa206163dd289e26ed4b8a7354bfa811460
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-04-27 14:42:18 +0100

    More mobile friendly formatting

--- lib/picos_sync/picos_sync.mli
+++ lib/picos_sync/picos_sync.mli
@@ -172,9 +172,9 @@ end
     condition variables:
 
     {[
-      module Bounded_queue : sig
+      module Bounded_q : sig
         type 'a t
-        val create : ?capacity:int -> unit -> 'a t
+        val create : capacity:int -> 'a t
         val push : 'a t -> 'a -> unit
         val pop : 'a t -> 'a
       end = struct
@@ -186,9 +186,9 @@ end
           not_full : Condition.t;
         }
 
-        let create ?(capacity = Int.max_int) () =
+        let create ~capacity =
           if capacity < 0 then
-            invalid_arg "capacity cannot be negative"
+            invalid_arg "negative capacity"
           else {
             mutex = Mutex.create ();
             queue = Queue.create ();
@@ -197,27 +197,33 @@ end
             not_full = Condition.create ();
           }
 
+        let is_full_unsafe t =
+          t.capacity <= Queue.length t.queue
+
         let push t x =
           let was_empty =
             Mutex.protect t.mutex @@ fun () ->
-            while t.capacity <= Queue.length t.queue do
+            while is_full_unsafe t do
               Condition.wait t.not_full t.mutex
             done;
             Queue.push x t.queue;
             Queue.length t.queue = 1
           in
-          if was_empty then Condition.signal t.not_empty
+          if was_empty then
+            Condition.signal t.not_empty
 
         let pop t =
           let elem, was_full =
             Mutex.protect t.mutex @@ fun () ->
             while Queue.length t.queue = 0 do
-              Condition.wait t.not_empty t.mutex
+              Condition.wait
+                t.not_empty t.mutex
             done;
-            let was_full = Queue.length t.queue = t.capacity in
+            let was_full = is_full_unsafe t in
             Queue.pop t.queue, was_full
           in
-          if was_full then Condition.signal t.not_full;
+          if was_full then
+            Condition.signal t.not_full;
           elem
       end
     ]}
@@ -228,18 +234,21 @@ end
     {[
       # Picos_fifos.run ~forbid:false @@ fun () ->
 
-        let bq = Bounded_queue.create ~capacity:3 () in
+        let bq =
+          Bounded_q.create ~capacity:3
+        in
 
         Bundle.join_after begin fun bundle ->
           Bundle.fork bundle begin fun () ->
             while true do
-              Printf.printf "Popped %d\n%!" (Bounded_queue.pop bq)
+              Printf.printf "Popped %d\n%!"
+                (Bounded_q.pop bq)
             done
           end;
 
           for i=1 to 5 do
             Printf.printf "Pushing %d\n%!" i;
-            Bounded_queue.push bq i
+            Bounded_q.push bq i
           done;
 
           Printf.printf "All done?\n%!";
@@ -250,9 +259,10 @@ end
         end;
 
         Printf.printf "Pushing %d\n%!" 101;
-        Bounded_queue.push bq 101;
+        Bounded_q.push bq 101;
 
-        Printf.printf "Popped %d\n%!" (Bounded_queue.pop bq)
+        Printf.printf "Popped %d\n%!"
+          (Bounded_q.pop bq)
       Pushing 1
       Pushing 2
       Pushing 3
