fd13ea65b6a15853adddf47fa985571e5ef60c56
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-05-02 16:18:28 +0300

    Move examples to bottom of page

--- lib/picos_htbl/picos_htbl.mli
+++ lib/picos_htbl/picos_htbl.mli
@@ -1,6 +1,71 @@
 (** Lock-free hash table.
 
-    Example:
+    🏎️ Single key reads with this hash table are actually wait-free rather than
+    just lock-free.  Internal resizing automatically uses all the threads that
+    are trying to write to the hash table. *)
+
+(** {1 API} *)
+
+type (!'k, !'v) t
+(** Represents a lock-free hash table mapping keys of type ['k] to values of
+    type ['v]. *)
+
+type 'k hashed_type = (module Stdlib.Hashtbl.HashedType with type t = 'k)
+(** First-class module type abbreviation. *)
+
+val create : ?hashed_type:'k hashed_type -> unit -> ('k, 'v) t
+(** [create ~hashed_type:(module Key) ()] creates a new empty lock-free hash
+    table.
+
+    The optional [hashed_type] argument can be used to specify the [equal] and
+    [hash] operations on keys.  Slow polymorphic equality [(=)] and slow
+    polymorphic {{!Stdlib.Hashtbl.seeded_hash} [seeded_hash (Random.bits ())]}
+    is used by default. *)
+
+(** {2 Looking up bindings} *)
+
+val find_exn : ('k, 'v) t -> 'k -> 'v
+(** [find_exn htbl key] returns the current binding of [key] in the hash table
+    [htbl] or raises {!Not_found} if no such binding exists. *)
+
+val mem : ('k, 'v) t -> 'k -> bool
+(** [mem htbl key] determines whether the hash table [htbl] has a binding for
+    the [key]. *)
+
+val to_seq : ('k, 'v) t -> ('k * 'v) Seq.t
+(** [to_seq htbl] takes a snapshot of the bindings in the hash table and returns
+    them as an association sequence.
+
+    🐌 This is a linear time operation. *)
+
+(** {2 Adding bindings} *)
+
+val try_add : ('k, 'v) t -> 'k -> 'v -> bool
+(** [try_add htbl key value] tries to add a new binding of [key] to [value] to
+    the hash table [htbl]. Returns [true] on success and [false] in case the
+    hash table already contained a binding for [key]. *)
+
+(** {2 Removing bindings} *)
+
+val try_remove : ('k, 'v) t -> 'k -> bool
+(** [try_remove htbl key] tries to remove a binding of [key] from the hash table
+    [htbl].  Returns [true] on success and [false] in case the hash table did
+    not contain a binding for [key]. *)
+
+val remove_exn : ('k, 'v) t -> 'k -> 'v
+(** [remove_exn htbl key] tries to remove a binding of [key] from the hash table
+    [htbl] and return it or raise {!Not_found} if no such binding exists. *)
+
+val remove_all : ('k, 'v) t -> ('k * 'v) Seq.t
+(** [remove_all htbl] takes a snapshot of the bindings in the hash table,
+    removes the bindings from the hash table, and returns the snapshot as an
+    association sequence.
+
+    🐌 This is a linear time operation. *)
+
+(** {1 Examples}
+
+    An example top-level session:
     {[
       # let t : (int, string) Picos_htbl.t =
           Picos_htbl.create
@@ -21,59 +86,4 @@
 
       # Picos_htbl.remove_all t |> List.of_seq
       - : (int * string) list = [(101, "Basics"); (42, "The answer")]
-    ]}
-
-    🏎️ Single key reads with this hash table are actually wait-free rather than
-    just lock-free.  Internal resizing automatically uses all the threads that
-    are trying to write to the hash table. *)
-
-type (!'k, !'v) t
-(** Represents a lock-free hash table mapping keys of type ['k] to values of
-    type ['v]. *)
-
-type 'k hashed_type = (module Stdlib.Hashtbl.HashedType with type t = 'k)
-(** First-class module type abbreviation. *)
-
-val create : ?hashed_type:'k hashed_type -> unit -> ('k, 'v) t
-(** [create ~hashed_type:(module Key) ()] creates a new empty lock-free hash
-    table.
-
-    The optional [hashed_type] argument can be used to specify the [equal] and
-    [hash] operations on keys.  Slow polymorphic equality [(=)] and slow
-    polymorphic {{!Stdlib.Hashtbl.seeded_hash} [seeded_hash (Random.bits ())]}
-    is used by default. *)
-
-val find_exn : ('k, 'v) t -> 'k -> 'v
-(** [find_exn htbl key] returns the current binding of [key] in the hash table
-    [htbl] or raises {!Not_found} if no such binding exists. *)
-
-val mem : ('k, 'v) t -> 'k -> bool
-(** [mem htbl key] determines whether the hash table [htbl] has a binding for
-    the [key]. *)
-
-val try_add : ('k, 'v) t -> 'k -> 'v -> bool
-(** [try_add htbl key value] tries to add a new binding of [key] to [value] to
-    the hash table [htbl]. Returns [true] on success and [false] in case the
-    hash table already contained a binding for [key]. *)
-
-val try_remove : ('k, 'v) t -> 'k -> bool
-(** [try_remove htbl key] tries to remove a binding of [key] from the hash table
-    [htbl].  Returns [true] on success and [false] in case the hash table did
-    not contain a binding for [key]. *)
-
-val remove_exn : ('k, 'v) t -> 'k -> 'v
-(** [remove_exn htbl key] tries to remove a binding of [key] from the hash table
-    [htbl] and return it or raise {!Not_found} if no such binding exists. *)
-
-val remove_all : ('k, 'v) t -> ('k * 'v) Seq.t
-(** [remove_all htbl] takes a snapshot of the bindings in the hash table,
-    removes the bindings from the hash table, and returns the snapshot as an
-    association sequence.
-
-    🐌 This is a linear time operation. *)
-
-val to_seq : ('k, 'v) t -> ('k * 'v) Seq.t
-(** [to_seq htbl] takes a snapshot of the bindings in the hash table and returns
-    them as an association sequence.
-
-    🐌 This is a linear time operation. *)
+    ]} *)
--- lib/picos_mpsc_queue/picos_mpsc_queue.mli
+++ lib/picos_mpsc_queue/picos_mpsc_queue.mli
@@ -1,6 +1,47 @@
 (** Lock-free multi-producer, single-consumer queue.
 
-    Example:
+    🏎️ This data structure is optimized for use as a scheduler's ready queue. *)
+
+(** {1 API} *)
+
+type !'a t
+(** A multi-producer, single-consumer queue. *)
+
+val create : unit -> 'a t
+(** [create ()] returns a new empty multi-producer, single-consumer queue. *)
+
+(** {2 Interface for producers}
+
+    ℹ️ The operations in this section can be called by both any number of
+    producers and the single owner / consumer of the queue. *)
+
+val push : 'a t -> 'a -> unit
+(** [push queue value] adds the [value] to the tail of the [queue]. *)
+
+val push_head : 'a t -> 'a -> unit
+(** [push_head queue value] adds the [value] to the head of the [queue]. *)
+
+(** {2 Interface for the owner / consumer}
+
+    ⚠️ The operations in this section should only be called by the single owner /
+    consumer of the queue. *)
+
+exception Empty
+(** Raised by {!pop_exn} in case it finds the queue empty. *)
+
+val pop_exn : 'a t -> 'a
+(** [pop_exn queue] tries to remove the value at the head of the [queue].
+    Returns the removed value or raises {!Empty} in case the queue was empty.
+
+    @raise Empty in case the queue was empty. *)
+
+val pop_all : 'a t -> 'a Seq.t
+(** [pop_all queue] removes all values from the [queue] and returns them as a
+    sequence. *)
+
+(** {1 Examples}
+
+    An example top-level session:
     {[
       # let q : int Picos_mpsc_queue.t =
           Picos_mpsc_queue.create ()
@@ -20,41 +61,4 @@
 
       # Picos_mpsc_queue.pop_all q |> List.of_seq
       - : int list = [42; 101]
-    ]}
-
-    🏎️ This data structure is optimized for use as a scheduler's ready queue. *)
-
-type !'a t
-(** A multi-producer, single-consumer queue. *)
-
-val create : unit -> 'a t
-(** [create ()] returns a new empty multi-producer, single-consumer queue. *)
-
-(** {1 Interface for producers}
-
-    ℹ️ The operations in this section can be called by both any number of
-    producers and the single owner / consumer of the queue. *)
-
-val push : 'a t -> 'a -> unit
-(** [push queue value] adds the [value] to the tail of the [queue]. *)
-
-val push_head : 'a t -> 'a -> unit
-(** [push_head queue value] adds the [value] to the head of the [queue]. *)
-
-(** {1 Interface for the owner / consumer}
-
-    ⚠️ The operations in this section should only be called by the single owner /
-    consumer of the queue. *)
-
-exception Empty
-(** Raised by {!pop_exn} in case it finds the queue empty. *)
-
-val pop_exn : 'a t -> 'a
-(** [pop_exn queue] tries to remove the value at the head of the [queue].
-    Returns the removed value or raises {!Empty} in case the queue was empty.
-
-    @raise Empty in case the queue was empty. *)
-
-val pop_all : 'a t -> 'a Seq.t
-(** [pop_all queue] removes all values from the [queue] and returns them as a
-    sequence. *)
+    ]} *)
