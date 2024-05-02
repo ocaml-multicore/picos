b619a9b56a88c5cbb636eb77bb833f297ffe48b4
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-05-02 09:57:41 +0300

    Fine tune MPSC queue docs

--- lib/picos_mpsc_queue/picos_mpsc_queue.mli
+++ lib/picos_mpsc_queue/picos_mpsc_queue.mli
@@ -1,4 +1,26 @@
-(** Multi-producer, single-consumer queue.
+(** Lock-free multi-producer, single-consumer queue.
+
+    Example:
+    {[
+      # let q : int Picos_mpsc_queue.t =
+          Picos_mpsc_queue.create ()
+      val q : int Picos_mpsc_queue.t = <abstr>
+
+      # Picos_mpsc_queue.push q 42
+      - : unit = ()
+
+      # Picos_mpsc_queue.push_head q 76
+      - : unit = ()
+
+      # Picos_mpsc_queue.push q 101
+      - : unit = ()
+
+      # Picos_mpsc_queue.pop_exn q
+      - : int = 76
+
+      # Picos_mpsc_queue.pop_all q |> List.of_seq
+      - : int list = [42; 101]
+    ]}
 
     🏎️ This data structure is optimized for use as a scheduler's ready queue. *)
 
@@ -8,7 +30,10 @@ type !'a t
 val create : unit -> 'a t
 (** [create ()] returns a new empty multi-producer, single-consumer queue. *)
 
-(** {2 Interface for producers} *)
+(** {1 Interface for producers}
+
+    ℹ️ The operations in this section can be called by both any number of
+    producers and the single owner / consumer of the queue. *)
 
 val push : 'a t -> 'a -> unit
 (** [push queue value] adds the [value] to the tail of the [queue]. *)
@@ -16,7 +41,10 @@ val push : 'a t -> 'a -> unit
 val push_head : 'a t -> 'a -> unit
 (** [push_head queue value] adds the [value] to the head of the [queue]. *)
 
-(** {2 Interface for the owner / consumer} *)
+(** {1 Interface for the owner / consumer}
+
+    ⚠️ The operations in this section should only be called by the single owner /
+    consumer of the queue. *)
 
 exception Empty
 (** Raised by {!pop_exn} in case it finds the queue empty. *)
@@ -24,8 +52,6 @@ exception Empty
 val pop_exn : 'a t -> 'a
 (** [pop_exn queue] tries to remove the value at the head of the [queue].
     Returns the removed value or raises {!Empty} in case the queue was empty.
-
-    ⚠️ This should only be called by the owner / consumer of the queue.
 
     @raise Empty in case the queue was empty. *)
 
