5f3a7068927b4cae8d2196a3f07a8542d5fb5112
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-05-29 00:14:22 +0300

    Initialize hash table on each benchmark round

--- bench/bench_htbl.ml
+++ bench/bench_htbl.ml
@@ -1,9 +1,10 @@
 open Multicore_bench
 module Htbl = Picos_htbl
 
-module Int = struct
-  include Int
+module Key = struct
+  type t = int
 
+  let equal = Int.equal
   let hash = Fun.id
 end
 
@@ -16,52 +17,44 @@ let run_one ~budgetf ~n_domains ?(n_ops = 400 * Util.iter_factor)
   assert (0 <= limit_mem && limit_mem <= 100);
   assert (limit_mem <= limit_add && limit_add <= 100);
 
-  let t = Htbl.create ~hashed_type:(module Int) () in
-
-  if prepopulate then
-    for _ = 1 to n_keys do
-      let value = Random.bits () in
-      let key = value mod n_keys in
-      Htbl.try_add t key value |> ignore
-    done;
+  let t = Htbl.create ~hashed_type:(module Key) () in
 
   let n_ops = (100 + percent_mem) * n_ops / 100 in
   let n_ops = n_ops * n_domains in
 
   let n_ops_todo = Atomic.make 0 |> Multicore_magic.copy_as_padded in
 
-  let init _ =
-    Atomic.set n_ops_todo n_ops;
-    Random.State.make_self_init ()
+  let before () =
+    let _ : _ Seq.t = Htbl.remove_all t in
+    Atomic.set n_ops_todo n_ops
+  in
+  let init i =
+    let state = Random.State.make_self_init () in
+    if prepopulate then begin
+      let n = ((i + 1) * n_keys / n_domains) - (i * n_keys / n_domains) in
+      for _ = 1 to n do
+        let value = Random.State.bits state in
+        let key = value mod n_keys in
+        Htbl.try_add t key value |> ignore
+      done
+    end;
+    state
   in
   let work _ state =
     let rec work () =
       let n = Util.alloc n_ops_todo in
-      if n <> 0 then
-        let rec loop n =
-          if 0 < n then
-            let value = Random.State.bits state in
-            let op = (value asr 20) mod 100 in
-            let key = value mod n_keys in
-            if op < percent_mem then begin
-              begin
-                match Htbl.find_exn t key with
-                | _ -> ()
-                | exception Not_found -> ()
-              end;
-              loop (n - 1)
-            end
-            else if op < limit_add then begin
-              Htbl.try_add t key value |> ignore;
-              loop (n - 1)
-            end
-            else begin
-              Htbl.try_remove t key |> ignore;
-              loop (n - 1)
-            end
-          else work ()
-        in
-        loop n
+      if n <> 0 then begin
+        for _ = 1 to n do
+          let value = Random.State.bits state in
+          let op = (value asr 20) mod 100 in
+          let key = value mod n_keys in
+          if op < percent_mem then
+            match Htbl.find_exn t key with _ -> () | exception Not_found -> ()
+          else if op < limit_add then Htbl.try_add t key value |> ignore
+          else Htbl.try_remove t key |> ignore
+        done;
+        work ()
+      end
     in
     work ()
   in
@@ -71,7 +64,7 @@ let run_one ~budgetf ~n_domains ?(n_ops = 400 * Util.iter_factor)
       (if n_domains = 1 then "" else "s")
       percent_mem
   in
-  Times.record ~budgetf ~n_domains ~init ~work ()
+  Times.record ~budgetf ~n_domains ~before ~init ~work ()
   |> Times.to_thruput_metrics ~n:n_ops ~singular:"operation" ~config
 
 let run_suite ~budgetf =
