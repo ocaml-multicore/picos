019fdb318e500fd258ebb6d6bbfebc86ab4f6563
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-07-31 13:10:24 +0300

    Set random seed for each QCheck STM round

--- test/stm_run/util.ml
+++ test/stm_run/util.ml
@@ -1,8 +1,9 @@
 let run_with_budget ~budgetf ~count run =
+  let state = Random.State.make_self_init () in
   let start = Unix.gettimeofday () in
   let rec loop ~total n =
     let current = Unix.gettimeofday () in
-    if current -. start <= budgetf && total < count then
+    if current -. start <= budgetf && total < count then begin
       let count =
         if total = 0 then n
         else
@@ -12,8 +13,11 @@ let run_with_budget ~budgetf ~count run =
           in
           Int.min (Int.min n (count - total)) max_count |> Int.max 32
       in
+      let seed = Random.State.full_int state Int.max_int in
+      QCheck_base_runner.set_seed seed;
       let error_code = run count in
       if error_code = 0 then loop ~total:(total + count) (n * 2) else error_code
+    end
     else 0
   in
   loop ~total:0 32
