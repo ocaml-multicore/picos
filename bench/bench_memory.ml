a1ede957f8dd1aa3601a67b4fa20e0bd800af470
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-08-07 10:32:32 +0300

    Add a benchmark of memory usage

---
+++ bench/bench_memory.ml
@@ -0,0 +1,68 @@
+open Multicore_bench
+
+let is_ocaml4 = String.starts_with ~prefix:"4." Sys.ocaml_version
+let bytes_per_word = if Sys.int_size <= 32 then 4 else 8
+
+let measure_live_bytes action =
+  let before = Gc.stat (Gc.full_major ()) in
+  action ();
+  let after = Gc.stat (Gc.full_major ()) in
+  (after.live_words - before.live_words) * bytes_per_word
+
+let run_suite ~budgetf:_ =
+  if is_ocaml4 then []
+  else
+    [
+      begin
+        Scheduler.run @@ fun () ->
+        let open Picos_structured in
+        Bundle.join_after @@ fun bundle ->
+        let n = 10_000 in
+        let bytes =
+          measure_live_bytes @@ fun () ->
+          let main () =
+            while true do
+              Control.yield ()
+            done
+          in
+          for _ = 1 to n do
+            Bundle.fork bundle main
+          done;
+          Control.yield ()
+        in
+        Bundle.terminate bundle;
+        Metric.make ~metric:"memory used" ~config:"fiber in a bundle" ~units:"B"
+          ~trend:`Lower_is_better ~description:"Memory usage"
+          (`Float (Float.of_int (bytes / n)))
+      end;
+      begin
+        Scheduler.run @@ fun () ->
+        let open Picos in
+        let open Picos_sync in
+        let computation = Computation.create () in
+        let n = 10_000 in
+        let latch = Latch.create 1 in
+        let bytes =
+          measure_live_bytes @@ fun () ->
+          let main () =
+            try
+              Latch.incr latch;
+              while true do
+                Fiber.yield ()
+              done
+            with Exit -> Latch.decr latch
+          in
+          for _ = 1 to n do
+            Fiber.spawn ~forbid:false computation [ main ]
+          done;
+          Fiber.yield ()
+        in
+        Computation.cancel computation (Exn_bt.get_callstack 0 Exit);
+        Latch.decr latch;
+        Latch.await latch;
+        Metric.make ~metric:"memory used"
+          ~config:"fiber with shared computation & latch" ~units:"B"
+          ~trend:`Lower_is_better ~description:"Memory usage"
+          (`Float (Float.of_int (bytes / n)))
+      end;
+    ]
