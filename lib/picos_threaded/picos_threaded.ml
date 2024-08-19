ocaml-multicore/picos

commit 636efcfff662d82c18f8ff8155109fd5740cf928

Redesign `Spawn` and introduce concept of fatal errors

lib/picos_threaded/picos_threaded.ml
@@ -1,12 +1,15 @@
 open Picos
 
-type t = { fiber : Fiber.t; mutex : Mutex.t; condition : Condition.t }
+type t = {
+  fiber : Fiber.t;
+  mutex : Mutex.t;
+  condition : Condition.t;
+  fatal_exn_handler : exn -> unit;
+}
 
-let create_packed ~forbid packed =
-  let fiber = Fiber.create_packed ~forbid packed in
-  let mutex = Mutex.create () in
-  let condition = Condition.create () in
-  { fiber; mutex; condition }
+let create ~fatal_exn_handler fiber =
+  let mutex = Mutex.create () and condition = Condition.create () in
+  { fiber; mutex; condition; fatal_exn_handler }
 
 let rec block trigger t =
   if not (Trigger.is_signaled trigger) then begin
@@ -45,6 +48,14 @@ let resume trigger t _ =
   end;
   Condition.broadcast t.condition
 
+let default_fatal_exn_handler exn =
+  prerr_string "Fatal error: exception ";
+  prerr_string (Printexc.to_string exn);
+  prerr_char '\n';
+  Printexc.print_backtrace stderr;
+  flush stderr;
+  exit 2
+
 let[@alert "-handler"] rec await t trigger =
   if Fiber.try_suspend t.fiber trigger t t resume then block trigger t;
   Fiber.canceled t.fiber
@@ -67,79 +78,31 @@ and cancel_after : type a. _ -> a Computation.t -> _ =
   Fiber.check t.fiber;
   Select.cancel_after computation ~seconds exn_bt
 
-and spawn : type a. _ -> forbid:bool -> a Computation.t -> _ =
- fun t ~forbid computation mains ->
+and spawn t fiber main =
   Fiber.check t.fiber;
-  let packed = Computation.Packed computation in
-  match mains with
-  | [ main ] ->
-      Thread.create
-        (fun () ->
-          (* We need to (recursively) install the handler on each new thread
-             that we create. *)
-          Handler.using handler (create_packed ~forbid packed) main)
-        ()
-      |> ignore
-  | mains -> begin
-      (* We try to be careful to implement the all-or-nothing behaviour based on
-         the assumption that we may run out of threads well before we run out of
-         memory.  In a thread pool based scheduler this should actually not
-         require special treatment. *)
-      let all_or_nothing = ref `Wait in
-      match
-        mains
-        |> List.iter @@ fun main ->
-           Thread.create
-             (fun () ->
-               if !all_or_nothing == `Wait then begin
-                 Mutex.lock t.mutex;
-                 match
-                   while
-                     match !all_or_nothing with
-                     | `Wait ->
-                         Condition.wait t.condition t.mutex;
-                         true
-                     | `All | `Nothing -> false
-                   do
-                     ()
-                   done
-                 with
-                 | () -> Mutex.unlock t.mutex
-                 | exception async_exn ->
-                     (* Condition.wait may be interrupted by asynchronous
-                        exceptions and we must make sure to unlock even in that
-                        case. *)
-                     Mutex.unlock t.mutex;
-                     raise async_exn
-               end;
-               if !all_or_nothing == `All then
-                 (* We need to (recursively) install the handler on each new
-                    thread that we create. *)
-                 Handler.using handler (create_packed ~forbid packed) main)
-             ()
-           |> ignore
-      with
-      | () ->
-          Mutex.lock t.mutex;
-          all_or_nothing := `All;
-          Mutex.unlock t.mutex;
-          Condition.broadcast t.condition
-      | exception exn ->
-          Mutex.lock t.mutex;
-          all_or_nothing := `Nothing;
-          Mutex.unlock t.mutex;
-          Condition.broadcast t.condition;
-          raise exn
-    end
+  match Thread.create start (fiber, t.fatal_exn_handler, main) with
+  | _ -> ( (* We assume that [main] is now guaranteed to be called. *) )
+  | exception exn ->
+      let exn_bt = Exn_bt.get exn in
+      (* [main] wasn't called, so we need to finalize. *)
+      let (Packed computation) = Fiber.get_computation fiber in
+      Computation.cancel computation exn_bt;
+      Exn_bt.raise exn_bt
 
 and handler = Handler.{ current; spawn; yield; cancel_after; await }
 
-let run ?(forbid = false) main =
+and start (fiber, fatal_exn_handler, main) =
+  (* We need to install the handler on each new thread that we create. *)
+  try Handler.using handler (create ~fatal_exn_handler fiber) main
+  with exn -> fatal_exn_handler exn
+
+let run_fiber ?(fatal_exn_handler = default_fatal_exn_handler) fiber main =
   Select.check_configured ();
+  Handler.using handler (create ~fatal_exn_handler fiber) main
+
+let run ?(forbid = false) ?fatal_exn_handler main =
   let computation = Computation.create ~mode:`LIFO () in
-  let context = create_packed ~forbid (Packed computation) in
-  let main () =
-    Computation.capture computation main ();
-    Computation.await computation
-  in
-  Handler.using handler context main
+  let fiber = Fiber.create ~forbid computation in
+  let main _ = Computation.capture computation main () in
+  run_fiber ?fatal_exn_handler fiber main;
+  Computation.await computation

bench/bench_fib.ocaml5.ml
@@ -8,8 +8,9 @@ let rec exp_fib i =
   if i < 2 then i
   else
     let computation = Computation.create ~mode:`LIFO () in
-    let main () = Computation.return computation (exp_fib (i - 2)) in
-    Fiber.spawn ~forbid:false computation [ main ];
+    let fiber = Fiber.create ~forbid:false computation in
+    let main _ = Computation.capture computation exp_fib (i - 2) in
+    Fiber.spawn fiber main;
     let f1 = exp_fib (i - 1) in
     let f2 = Computation.await computation in
     f1 + f2

bench/bench_memory.ml
@@ -112,7 +112,7 @@ let run_suite ~budgetf:_ =
         let latch = Latch.create 1 in
         let bytes =
           measure_live_bytes @@ fun () ->
-          let main () =
+          let main _ =
             try
               Latch.incr latch;
               while true do
@@ -121,7 +121,7 @@ let run_suite ~budgetf:_ =
             with Exit -> Latch.decr latch
           in
           for _ = 1 to n do
-            Fiber.spawn ~forbid:false computation [ main ]
+            Fiber.spawn (Fiber.create ~forbid:false computation) main
           done;
           Fiber.yield ()
         in

bench/bench_spawn.ml
@@ -5,7 +5,7 @@ let factor =
   Util.iter_factor
   * if String.starts_with ~prefix:"4." Sys.ocaml_version then 1 else 10
 
-let run_one ~budgetf ~at_a_time () =
+let run_one ~budgetf () =
   let n_spawns = 10 * factor in
 
   let init _ = () in
@@ -13,25 +13,22 @@ let run_one ~budgetf ~at_a_time () =
   let work _ () =
     let counter = ref n_spawns in
     let computation = Computation.create ~mode:`LIFO () in
-    for _ = 1 to n_spawns / at_a_time do
-      let main () =
-        let n = !counter - 1 in
-        counter := n;
-        if n = 0 then Computation.finish computation
-      in
-      let mains = List.init at_a_time @@ fun _ -> main in
-      Fiber.spawn ~forbid:false computation mains
+    let computation_packed = Computation.Packed computation in
+    let main _ =
+      let n = !counter - 1 in
+      counter := n;
+      if n = 0 then Computation.finish computation
+    in
+    for _ = 1 to n_spawns do
+      let fiber = Fiber.create_packed ~forbid:false computation_packed in
+      Fiber.spawn fiber main
     done;
     Computation.await computation
   in
 
-  let config = Printf.sprintf "%d at a time" at_a_time in
+  let config = "with packed computation" in
   Times.record ~budgetf ~n_domains:1 ~n_warmups:1 ~n_runs_min:1 ~init ~wrap
     ~work ()
   |> Times.to_thruput_metrics ~n:n_spawns ~singular:"spawn" ~config
 
-let run_suite ~budgetf =
-  if Sys.int_size <= 32 then []
-  else
-    [ 1; 2; 4; 8 ]
-    |> List.concat_map @@ fun at_a_time -> run_one ~budgetf ~at_a_time ()
+let run_suite ~budgetf = if Sys.int_size <= 32 then [] else run_one ~budgetf ()

lib/picos/bootstrap/picos_bootstrap.ml
@@ -385,8 +385,7 @@ end
 module Handler = struct
   type 'c t = {
     current : 'c -> Fiber.t;
-    spawn :
-      'a. 'c -> forbid:bool -> 'a Computation.t -> (unit -> unit) list -> unit;
+    spawn : 'c -> Fiber.t -> (Fiber.t -> unit) -> unit;
     yield : 'c -> unit;
     cancel_after :
       'a. 'c -> 'a Computation.t -> seconds:float -> Exn_bt.t -> unit;

lib/picos/intf.ocaml5.ml
@@ -132,13 +132,13 @@ module type Fiber = sig
       associated with the fiber has been canceled the scheduler is free to
       discontinue the fiber immediately before spawning new fibers.
 
-      The scheduler is free to run the newly created fibers on any domain and
+      The scheduler is free to run the newly created fiber on any domain and
       decide which fiber to give priority to.
 
-      ⚠️ The scheduler should guarantee that, when [Spawn] returns normally, all
-      of the [mains] will eventually be called by the scheduler and, when
-      [Spawn] raises an exception, none of the [mains] will be called.  In other
-      words, [Spawn] should check cancelation just once and be all or nothing.
+      ⚠️ The scheduler should guarantee that, when [Spawn] returns normally, the
+      given [main] will eventually be called by the scheduler and, when [Spawn]
+      raises an exception, the [main] will not be called.  In other words,
+      [Spawn] should check cancelation just once and be all or nothing.
       Furthermore, in case a newly spawned fiber is canceled before its main is
       called, the scheduler must still call the main.  This allows a program to
       ensure, i.e. keep track of, that all fibers it spawns are terminated
@@ -146,10 +146,5 @@ module type Fiber = sig
       properly. *)
   type _ Effect.t +=
     private
-    | Spawn : {
-        forbid : bool;
-        computation : 'a computation;
-        mains : (unit -> unit) list;
-      }
-        -> unit Effect.t
+    | Spawn : { fiber : t; main : t -> unit } -> unit Effect.t
 end

lib/picos/ocaml4/picos_ocaml.ml
@@ -1,26 +1,26 @@
 open Picos_bootstrap
 
-let error () =
+let[@inline never] error _ =
   raise (Sys_error "Picos.Handler.using not called for current thread")
 
 module Handler = struct
   type entry = E : { context : 'a; handler : 'a Handler.t } -> entry
 
   let default =
-    let current _ = error ()
-    and spawn _ ~forbid:_ _ _ = error ()
-    and yield _ = error ()
-    and cancel_after _ _ ~seconds:_ _ = error ()
-    and await _ _ = error () in
+    let current = error
+    and spawn _ _ = error
+    and yield = error
+    and cancel_after _ _ ~seconds:_ = error
+    and await _ = error in
     E { context = (); handler = { current; spawn; yield; cancel_after; await } }
 
   let key = Picos_thread.TLS.new_key @@ fun () -> default
   let get () = Picos_thread.TLS.get key
 
-  let using handler context thunk =
+  let using handler context main =
     let old = Picos_thread.TLS.get key in
     Picos_thread.TLS.set key (E { context; handler });
-    match thunk () with
+    match main (handler.current context) with
     | value ->
         Picos_thread.TLS.set key old;
         value
@@ -42,9 +42,9 @@ module Fiber = struct
     let (E r) = Handler.get () in
     r.handler.current r.context
 
-  let spawn ~forbid computation mains =
+  let spawn fiber main =
     let (E r) = Handler.get () in
-    r.handler.spawn r.context ~forbid computation mains
+    r.handler.spawn r.context fiber main
 
   let yield () =
     let (E r) = Handler.get () in

lib/picos/ocaml5/picos_ocaml.ml
@@ -25,15 +25,9 @@ module Fiber = struct
   let current () = Effect.perform Current
 
   type _ Effect.t +=
-    | Spawn : {
-        forbid : bool;
-        computation : 'a Computation.t;
-        mains : (unit -> unit) list;
-      }
-        -> unit Effect.t
+    | Spawn : { fiber : Fiber.t; main : Fiber.t -> unit } -> unit Effect.t
 
-  let spawn ~forbid computation mains =
-    Effect.perform @@ Spawn { forbid; computation; mains }
+  let spawn fiber main = Effect.perform @@ Spawn { fiber; main }
 
   type _ Effect.t += Yield : unit Effect.t
 
@@ -78,7 +72,7 @@ module Handler = struct
       | Fiber.Spawn r ->
           Some
             (fun k ->
-              match h.spawn c ~forbid:r.forbid r.computation r.mains with
+              match h.spawn c r.fiber r.main with
               | unit -> Effect.Deep.continue k unit
               | exception exn -> discontinue k exn)
       | Fiber.Yield -> yield
@@ -95,5 +89,5 @@ module Handler = struct
       | _ -> None
     in
     let handler = Effect.Deep.{ retc = Fun.id; exnc = raise; effc } in
-    fun main -> Effect.Deep.match_with main () handler
+    fun main -> Effect.Deep.match_with main (h.current c) handler
 end

commit 5fc67eba58767e1a7738c9824d0e7aa64ab4124a

Fix to not cancel computation In case of spawn failure
This seems to have been introduced in PR #210 and is not how spawn is specified
to behave.

lib/picos_threaded/picos_threaded.ml
@@ -80,14 +80,7 @@ and cancel_after : type a. _ -> a Computation.t -> _ =
 
 and spawn t fiber main =
   Fiber.check t.fiber;
-  match Thread.create start (fiber, t.fatal_exn_handler, main) with
-  | _ -> ( (* We assume that [main] is now guaranteed to be called. *) )
-  | exception exn ->
-      let exn_bt = Exn_bt.get exn in
-      (* [main] wasn't called, so we need to finalize. *)
-      let (Packed computation) = Fiber.get_computation fiber in
-      Computation.cancel computation exn_bt;
-      Exn_bt.raise exn_bt
+  Thread.create start (fiber, t.fatal_exn_handler, main) |> ignore
 
 and handler = Handler.{ current; spawn; yield; cancel_after; await }
 