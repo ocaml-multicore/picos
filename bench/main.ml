fb731125daa29a11d453c1a4bf02aca9f7cc4193
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-08-27 02:18:10 +0300

    Add `Picos_mpmcq` bench

--- bench/main.ml
+++ bench/main.ml
@@ -13,6 +13,7 @@ let benchmarks =
     ("Picos Yield", Bench_yield.run_suite);
     ("Picos Cancel_after with Picos_select", Bench_cancel_after.run_suite);
     ("Ref with Picos_sync.Mutex", Bench_ref_mutex.run_suite);
+    ("Picos_mpmcq", Bench_mpmcq.run_suite);
     ("Picos_mpscq", Bench_mpscq.run_suite);
     ("Picos_htbl", Bench_htbl.run_suite);
     ("Picos_stdio", Bench_stdio.run_suite);
