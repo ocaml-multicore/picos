f319fd0e990215d1b01cc94dbe20e2ba8234a7fc
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-09-03 20:26:41 +0300

    Instruct to install `picos_stdio`

--- lib/picos_mux.fifo/select.none.ml
+++ lib/picos_mux.fifo/select.none.ml
@@ -1,4 +1,4 @@
 let cancel_after _ ~seconds:_ _ =
-  raise (Sys_error "Computation: cancel_after unavailable")
+  raise (Sys_error "Computation: cancel_after unavailable; install picos_stdio")
 
 let check_configured = Fun.id
