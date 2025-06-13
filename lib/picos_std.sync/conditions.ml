bef8855d8e9a134ede4eed69554845d1f8d5a4e9
Vesa Karvonen <vesa.a.j.k@gmail.com>
2025-06-13 16:33:10 +0100

    Use a counter based condition variable implementation

--- lib/picos_std.sync/conditions.ml
+++ lib/picos_std.sync/conditions.ml
@@ -1,13 +1,12 @@
 open Picos
 open Picos_std_awaitable
 
-type t = unit Awaitable.t
+type t = int Awaitable.t
 
-let create ?padded () = Awaitable.make ?padded ()
+let create ?padded () = Awaitable.make ?padded 0
 
 let[@inline] wait t lock ~acquire ~release =
-  let trigger = Trigger.create () in
-  let awaiter = Awaitable.Awaiter.add t trigger in
+  let before = Awaitable.get t in
   release lock;
   let lock_forbidden lock =
     let fiber = Fiber.current () in
@@ -20,12 +19,17 @@ let[@inline] wait t lock ~acquire ~release =
         Fiber.set fiber ~forbid;
         Printexc.raise_with_backtrace exn bt
   in
-  match Trigger.await trigger with
-  | None -> lock_forbidden lock
-  | Some exn_bt ->
-      Awaitable.Awaiter.remove awaiter;
+  match if Awaitable.get t == before then Awaitable.await t before with
+  | () -> lock_forbidden lock
+  | exception exn ->
+      let bt = Printexc.get_raw_backtrace () in
       lock_forbidden lock;
-      Printexc.raise_with_backtrace (fst exn_bt) (snd exn_bt)
+      Printexc.raise_with_backtrace exn bt
 
-let signal = Awaitable.signal
-let broadcast = Awaitable.broadcast
+let signal t =
+  Awaitable.incr t;
+  Awaitable.signal t
+
+let broadcast t =
+  Awaitable.incr t;
+  Awaitable.broadcast t
