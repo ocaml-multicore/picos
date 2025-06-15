cd68168145df9b3b78eb1db6ecaa2e6634264c05
Vesa Karvonen <vesa.a.j.k@gmail.com>
2025-06-15 08:58:56 +0100

    Inline operations in ref lock benchmark

--- bench/bench_ref.ml
+++ bench/bench_ref.ml
@@ -17,75 +17,303 @@ module Ref = struct
     let before = !x in
     x := after;
     before
+
+  let incr = incr
+
+  let[@inline] swap x =
+    let l, r = !x in
+    x := (r, l)
+
+  let[@inline] push x = x := 101 :: !x
+  let[@inline] pop x = match !x with [] -> () | _ :: xs -> x := xs
 end
 
-type t =
-  | Op : string * 'a * ('a Ref.t -> _) * ('a Ref.t -> _) * [ `RW | `RO ] -> t
+type _ op =
+  | Get : int op
+  | Incr : int op
+  | Push_and_pop : int list op
+  | Cas_int : int op
+  | Xchg_int : int op
+  | Swap : (int * int) op
 
-let run_one ~budgetf ?(n_iter = 250 * Util.iter_factor) ~lock_type
-    (Op (name, value, op1, op2, op_kind)) =
+let run_one (type a) ~budgetf ?(n_iter = 250 * Util.iter_factor) ~lock_type
+    (op : a op) =
   let lock = Lock.create () in
   let sem = Sem.create 1 in
   let rwlock = Rwlock.create () in
+
+  let name, (value : a) =
+    match op with
+    | Get -> ("get", 42)
+    | Incr -> ("incr", 0)
+    | Push_and_pop -> ("push & pop", [])
+    | Cas_int -> ("cas int", 0)
+    | Xchg_int -> ("xchg int", 0)
+    | Swap -> ("swap", (4, 2))
+  in
 
   let loc = Ref.make value in
 
   let init _ = () in
   let wrap _ () = Scheduler.run in
   let work _ () =
-    match (lock_type, op_kind) with
-    | `Lock, _ ->
-        let rec loop i =
-          if i > 0 then begin
-            Lock.acquire lock;
-            op1 loc |> ignore;
-            Lock.release lock;
-            Lock.acquire lock;
-            op2 loc |> ignore;
-            Lock.release lock;
-            loop (i - 2)
-          end
-        in
-        loop n_iter
-    | `Rwlock, `RW ->
-        let rec loop i =
-          if i > 0 then begin
-            Rwlock.acquire rwlock;
-            op1 loc |> ignore;
-            Rwlock.release rwlock;
-            Rwlock.acquire rwlock;
-            op2 loc |> ignore;
-            Rwlock.release rwlock;
-            loop (i - 2)
-          end
-        in
-        loop n_iter
-    | `Rwlock, `RO ->
-        let rec loop i =
-          if i > 0 then begin
-            Rwlock.acquire_shared rwlock;
-            op1 loc |> ignore;
-            Rwlock.release_shared rwlock;
-            Rwlock.acquire_shared rwlock;
-            op2 loc |> ignore;
-            Rwlock.release_shared rwlock;
-            loop (i - 2)
-          end
-        in
-        loop n_iter
-    | `Sem, _ ->
-        let rec loop i =
-          if i > 0 then begin
-            Sem.acquire sem;
-            op1 loc |> ignore;
-            Sem.release sem;
-            Sem.acquire sem;
-            op2 loc |> ignore;
-            Sem.release sem;
-            loop (i - 2)
-          end
-        in
-        loop n_iter
+    match (lock_type, op) with
+    | `Lock, _ -> begin
+        let acquire = Lock.acquire and release = Lock.release and lock = lock in
+        match op with
+        | Get ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                let a = !(Sys.opaque_identity loc) in
+                release lock;
+                acquire lock;
+                let b = !(Sys.opaque_identity loc) in
+                release lock;
+                loop (i - 2 + (a - b))
+              end
+            in
+            loop n_iter
+        | Incr ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Push_and_pop ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.push loc;
+                release lock;
+                acquire lock;
+                Ref.pop loc |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Cas_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.compare_and_set loc 0 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.compare_and_set loc 1 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Xchg_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.exchange loc 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.exchange loc 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Swap ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+      end
+    | `Rwlock, Get -> begin
+        let acquire = Rwlock.acquire_shared
+        and release = Rwlock.release_shared
+        and lock = rwlock in
+        match op with
+        | Get ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                let a = !(Sys.opaque_identity loc) in
+                release lock;
+                acquire lock;
+                let b = !(Sys.opaque_identity loc) in
+                release lock;
+                loop (i - 2 + (a - b))
+              end
+            in
+            loop n_iter
+        | _ -> ()
+      end
+    | `Rwlock, _ -> begin
+        let acquire = Rwlock.acquire
+        and release = Rwlock.release
+        and lock = rwlock in
+        match op with
+        | Get -> ()
+        | Incr ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Push_and_pop ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.push loc;
+                release lock;
+                acquire lock;
+                Ref.pop loc |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Cas_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.compare_and_set loc 0 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.compare_and_set loc 1 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Xchg_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.exchange loc 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.exchange loc 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Swap ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+      end
+    | `Sem, _ -> begin
+        let acquire = Sem.acquire and release = Sem.release and lock = sem in
+        match op with
+        | Get ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                let a = !(Sys.opaque_identity loc) in
+                release lock;
+                acquire lock;
+                let b = !(Sys.opaque_identity loc) in
+                release lock;
+                loop (i - 2 + (a - b))
+              end
+            in
+            loop n_iter
+        | Incr ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                acquire lock;
+                Ref.incr loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Push_and_pop ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.push loc;
+                release lock;
+                acquire lock;
+                Ref.pop loc |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Cas_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.compare_and_set loc 0 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.compare_and_set loc 1 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Xchg_int ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.exchange loc 1 |> ignore;
+                release lock;
+                acquire lock;
+                Ref.exchange loc 0 |> ignore;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+        | Swap ->
+            let rec loop i =
+              if i > 0 then begin
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                acquire lock;
+                Ref.swap loc;
+                release lock;
+                loop (i - 2)
+              end
+            in
+            loop n_iter
+      end
   in
 
   let config =
@@ -99,24 +327,14 @@ let run_one ~budgetf ?(n_iter = 250 * Util.iter_factor) ~lock_type
   |> Times.to_thruput_metrics ~n:n_iter ~singular:"op" ~config
 
 let run_suite ~budgetf =
-  Util.cross [ `Lock; `Rwlock; `Sem ]
-    [
-      (let get x = !x in
-       Op ("get", 42, get, get, `RO));
-      (let incr x = x := !x + 1 in
-       Op ("incr", 0, incr, incr, `RW));
-      (let push x = x := 101 :: !x
-       and pop x = match !x with [] -> () | _ :: xs -> x := xs in
-       Op ("push & pop", [], push, pop, `RW));
-      (let cas01 x = Ref.compare_and_set x 0 1
-       and cas10 x = Ref.compare_and_set x 1 0 in
-       Op ("cas int", 0, cas01, cas10, `RW));
-      (let xchg1 x = Ref.exchange x 1 and xchg0 x = Ref.exchange x 0 in
-       Op ("xchg int", 0, xchg1, xchg0, `RW));
-      (let swap x =
-         let l, r = !x in
-         x := (r, l)
-       in
-       Op ("swap", (4, 2), swap, swap, `RW));
-    ]
-  |> List.concat_map @@ fun (lock_type, op) -> run_one ~budgetf ~lock_type op
+  [ `Lock; `Rwlock; `Sem ]
+  |> List.concat_map @@ fun lock_type ->
+     [
+       run_one ~budgetf ~lock_type Get;
+       run_one ~budgetf ~lock_type Incr;
+       run_one ~budgetf ~lock_type Push_and_pop;
+       run_one ~budgetf ~lock_type Cas_int;
+       run_one ~budgetf ~lock_type Xchg_int;
+       run_one ~budgetf ~lock_type Swap;
+     ]
+     |> List.concat
