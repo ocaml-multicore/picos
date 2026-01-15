778902cfff507a70d49edf160602466099cf053e
Vesa Karvonen <vesa.a.j.k@gmail.com>
2026-01-15 12:39:00 +0000

    Experimental more incremental resize approach

---
+++ bench/bench_ihtbl.ml
@@ -0,0 +1,109 @@
+open Multicore_bench
+module Htbl = Picos_aux_ihtbl
+
+module Key = struct
+  type t = int
+
+  let equal = Int.equal
+  let hash = Fun.id
+end
+
+let run_one ~budgetf ~n_domains ?(n_ops = 400 * Util.iter_factor)
+    ?(n_keys = 1000) ~percent_mem ?(percent_add = (100 - percent_mem + 1) / 2)
+    ?(prepopulate = true) () =
+  let limit_mem = percent_mem in
+  let limit_add = percent_mem + percent_add in
+
+  assert (0 <= limit_mem && limit_mem <= 100);
+  assert (limit_mem <= limit_add && limit_add <= 100);
+
+  let t = Htbl.create ~hashed_type:(module Key) () in
+
+  let n_ops = (100 + percent_mem) * n_ops / 100 in
+  let n_ops = n_ops * n_domains in
+
+  let n_ops_todo = Countdown.create ~n_domains () in
+
+  let before () =
+    Htbl.clear t;
+    assert (Htbl.non_linearizable_length t = 0);
+    Countdown.non_atomic_set n_ops_todo n_ops
+  in
+  let init i =
+    let state = Random.State.make_self_init () in
+    if prepopulate then begin
+      let n = ((i + 1) * n_keys / n_domains) - (i * n_keys / n_domains) in
+      for _ = 1 to n do
+        let value = Random.State.bits state in
+        let key = value mod n_keys in
+        Htbl.try_add t key value |> ignore
+      done
+    end;
+    state
+  in
+  let work domain_index state =
+    let rec work () =
+      let n = Countdown.alloc n_ops_todo ~domain_index ~batch:1000 in
+      if n <> 0 then begin
+        for _ = 1 to n do
+          let value = Random.State.bits state in
+          let op = (value asr 20) mod 100 in
+          let key = value mod n_keys in
+          if op < percent_mem then
+            match Htbl.find_exn t key with _ -> () | exception Not_found -> ()
+          else if op < limit_add then Htbl.try_add t key value |> ignore
+          else
+            match Htbl.remove_exn t key with
+            | _ -> ()
+            | exception Not_found -> ()
+        done;
+        work ()
+      end
+    in
+    work ()
+  in
+
+  let config =
+    Printf.sprintf "%d worker%s, %d%% reads" n_domains
+      (if n_domains = 1 then "" else "s")
+      percent_mem
+  in
+  Times.record ~budgetf ~n_domains ~before ~init ~work ()
+  |> Times.to_thruput_metrics ~n:n_ops ~singular:"operation" ~config
+
+let run_fill1m ~budgetf ~n_domains () =
+  let n_ops = 250_000 * n_domains in
+  let t = Htbl.create ~hashed_type:(module Key) () in
+  let before () =
+    Htbl.clear t;
+    assert (Htbl.non_linearizable_length t = 0)
+  in
+  let init i =
+    let k0 = i * n_ops / n_domains in
+    let k1 = ((i + 1) * n_ops / n_domains) - 1 in
+    (k0, k1)
+  in
+  let work _ (k0, k1) =
+    for k = k0 to k1 do
+      assert (Htbl.try_add t k ())
+    done
+  in
+  let after () = assert (n_ops = Htbl.non_linearizable_length t) in
+  let config =
+    Printf.sprintf "%d worker%s" n_domains (if n_domains = 1 then "" else "s")
+  in
+  Times.record ~n_runs_min:19 ~budgetf ~n_domains ~before ~init ~work ~after ()
+  |> Times.to_thruput_metrics ~n:n_ops ~singular:"add" ~config
+
+let run_suite ~budgetf =
+  [ 1; 2; 4; 8 ]
+  |> List.concat_map @@ fun n_domains ->
+     if Picos_domain.recommended_domain_count () < n_domains then []
+     else
+       let basic =
+         [ 10; 50; 90 ]
+         |> List.concat_map @@ fun percent_mem ->
+            run_one ~budgetf ~n_domains ~percent_mem ()
+       in
+       let fill1m = run_fill1m ~budgetf ~n_domains () in
+       basic @ fill1m
